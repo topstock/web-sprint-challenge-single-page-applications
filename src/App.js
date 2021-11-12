ab360126e65fd126db3908989838533672720944
Phil G <fillupgalbraith@gmail.com>
2021-11-12 11:58:40 -0800

    testing input changes

--- src/App.js
+++ src/App.js
@@ -1,33 +1,55 @@
 import React, { useState, useEffect } from "react";
 import UNSPLASH from "./constants/url-constants";
-import { NavLink, Route, useMatch, useHistory } from "react-router-dom";
+import { NavLink, Route, useHistory } from "react-router-dom";
 import Form from "./components/Form";
 import axios from "axios";
-import urlConstants from "./constants/url-constants";
+import schema from "./formSchema";
+import * as yup from "yup";
 
 const initialFormValues = {
-    nameOrder: "",
+    name: "",
     size: "",
     meat: false,
     vegitable: false,
-    crust: "",
-  
+    crust: ""  
 }
+
+const initialFormErrors = {
+  name: "",
+  size: "",
+  crust: ""
+}
+
 const initialDisabled = true;
 
 const App = () => {
   const { formValues, setFormValues } = useState(initialFormValues);
+  const { formErrors, setFormErrors } = useState(initialFormErrors);
   const { disabled, setDisabled } = useState(initialDisabled);
   const {UNSPLASH_URL, UNSPLASH_PROFILE_URL} = UNSPLASH;
   const history = useHistory();
-
+  
+  const validate = (name, value) => {
+    yup.reach(schema, name).validate(value)
+      .then(() => setFormErrors({ ...formErrors, [name]: '' }))
+      .catch(err => setFormErrors({ ...formErrors, [name]: err.errors[0]}))
+  }
   const submit = () => {
 
   }
 
-  const change = () => {
-
+  const changeInput = (name, value) => {
+    validate(name, value);
+    setFormValues({
+      ...formValues,
+      [name]: value
+    })
   }
+
+  useEffect(() => {
+    // 🔥 STEP 9- ADJUST THE STATUS OF `disabled` EVERY TIME `formValues` CHANGES
+    schema.isValid(formValues).then(valid => setDisabled(!valid));
+  }, [formValues])
 
   return (
     <>
@@ -54,7 +76,7 @@ const App = () => {
         <Route path="/pizza" >
           <Form 
             values={formValues} 
-            change={change}
+            change={changeInput}
             submit={submit}
             disabled={disabled}
             />
--- src/components/Form.js
+++ src/components/Form.js
@@ -16,11 +16,15 @@ const Form = (props) => {
   const onChange = evt => {
     const {name, value, checked, type } = evt.target;
     const knownValue = type === 'checkbox' ? checked : value;
-    change(name, knownValue)
+    change(name, knownValue);
   }
 
     return ( 
-        <form className="form container" onSubmit={onSubmit}>
+        <form 
+        className="form container" 
+        onSubmit={onSubmit}
+        id="pizza-form">
+
             <div className="form-top submit">
                 <h2>Pizza</h2>
                 <button disabled={disabled}>Submit Order</button>
@@ -31,45 +35,46 @@ const Form = (props) => {
                 <h3>Options</h3>
                 <label>
                     <input
-                      onChange={onChange}
-                      name="name"
-                      type="text"
-                    //   value={values.nameOrder}
+                        id="name-input"
+                        onChange={onChange}
+                        name="name"
+                        type="text"
+                       value={values.name}
                     />
                 </label>  
 
                 <label>Size
                     <input
-                      onChange={onChange}
-                      name="size"
-                      type="text"
+                        onChange={onChange}
+                        name="size"
+                        type="text"
                     //   value={values.size}
                     />
                 </label> 
 
                 <label>Meat 
                     <input
-                      onChange={onChange}
-                      name="meat"
-                      type="checkbox"
+                        onChange={onChange}
+                        name="meat"
+                        type="checkbox"
                     //   checked={values.meat}
                     />
                 </label>                    
                 
                 <label>Vegitable 
                     <input
-                      onChange={onChange}
-                      name="vegitable"
-                      type="checkbox"
+                        onChange={onChange}
+                        name="vegitable"
+                        type="checkbox"
                     //   checked={values.vegitable}
                     />
                 </label>
 
                 <label>Crust 
                     <input
-                       onChange={onChange}
-                       name="crust"
-                       type="text"
+                        onChange={onChange}
+                        name="crust"
+                        type="text"
                     //    value={values.crust}
                     />
                 </label>                    
